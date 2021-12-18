2061564395ca09b8d63089b7ea90be552a0147ba
Rui Leite <ruipimentelleite@gmail.com>
2021-12-18 13:44:45 -0300

    Cria template da ferramenta "Cronômetro".

---
+++ js/cronometro.js
@@ -0,0 +1,5 @@
+const botaoStart = document.querySelector('.btn.btn-success');
+const botaoPause = document.querySelector('.btn.btn-warning');
+const botaoStop = document.querySelector('.btn.btn-danger');
+
+const output = document.querySelector('output');
